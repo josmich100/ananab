c4d7ecb4d7916872b276a1a82326e86b3118cc90
ReuelO <reuenga@gmail.com>
2021-10-18 13:05:57 +0300

    erro r

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
+import Hero from "../components/Hero";
+import NavBar from "../components/NavBar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
