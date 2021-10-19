62adb3c235d8fdc9cb9bd1c0fae331ae9fe94f79
ReuelO <reuenga@gmail.com>
2021-10-19 15:46:14 +0300

    error className

--- components/Section.js
+++ components/Section.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const Section = (props) => {
   return (
-    <div classNameName={`max-w-screen-lg ${props.spacing} mx-auto px-3 pt-6`}>
+    <div className={`max-w-screen-lg ${props.spacing} mx-auto px-3 pt-6`}>
       {(props.title || props.description) && (
-        <div classNameName="mt-5 text-center">
+        <div className="mt-5 text-center">
           {props.title && (
-            <h2 classNameName="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+            <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
               {props.title}
             </h2>
           )}
           {props.description && (
-            <div classNameName="mt-4 text-xl md:px-20">{props.description}</div>
+            <div className="mt-4 text-xl md:px-20">{props.description}</div>
           )}
         </div>
       )}
